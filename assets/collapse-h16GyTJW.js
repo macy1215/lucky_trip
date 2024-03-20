59a3146b17c19416fc1f415c054deee4ec40f011
Macy Huang <mjmj3794@gmail.com>
2024-03-21 00:29:44 +0800

    Updates

--- assets/collapse-h16GyTJW.js
+++ assets/collapse-h16GyTJW.js
@@ -1,4 +1,4 @@
-import{I as P,J as F}from"./index--CWl1Wb0.js";import{r as H,a as V,b as q,c as z}from"./selector-engine-wStOV2id.js";var A={exports:{}};/*!
+import{I as P,J as F}from"./index-tObUnU7K.js";import{r as H,a as V,b as q,c as z}from"./selector-engine-_8PpKhyI.js";var A={exports:{}};/*!
   * Bootstrap collapse.js v5.3.2 (https://getbootstrap.com/)
   * Copyright 2011-2023 The Bootstrap Authors (https://github.com/twbs/bootstrap/graphs/contributors)
   * Licensed under MIT (https://github.com/twbs/bootstrap/blob/main/LICENSE)
