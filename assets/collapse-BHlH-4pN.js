9be19b9b2c31530decf6621b75d6473196cc31d1
Macy Huang <mjmj3794@gmail.com>
2024-03-23 09:20:28 +0800

    Updates

--- assets/collapse-BHlH-4pN.js
+++ assets/collapse-BHlH-4pN.js
@@ -1,4 +1,4 @@
-import{I as P,J as F}from"./index-Vr07P915.js";import{r as H,a as V,b as q,c as z}from"./selector-engine-5ekd4l2K.js";var A={exports:{}};/*!
+import{I as P,J as F}from"./index-O7lt0Cqn.js";import{r as H,a as V,b as q,c as z}from"./selector-engine-NILIjsWi.js";var A={exports:{}};/*!
   * Bootstrap collapse.js v5.3.2 (https://getbootstrap.com/)
   * Copyright 2011-2023 The Bootstrap Authors (https://github.com/twbs/bootstrap/graphs/contributors)
   * Licensed under MIT (https://github.com/twbs/bootstrap/blob/main/LICENSE)
