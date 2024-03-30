38fe32202b2aafe6b9b61bb45b08b7936fb0b5de
Macy Huang <mjmj3794@gmail.com>
2024-03-30 09:28:45 +0800

    首頁調整特效

--- src/views/user/UserHome.vue
+++ src/views/user/UserHome.vue
@@ -16,9 +16,12 @@
             text-white d-md-none text-start mobiltitle">
               好嗨遊與你一起輕鬆走！
             </h3>
-            <button type="button" class="my-md-3 my-1 btn btn-primary text-white btn-lg fw-bold">
-              探索行程
-            </button>
+            <RouterLink :to="`/products`">
+              <button type="button" class="my-md-3 my-1 btn btn-primary text-white
+                btn-lg fw-bold btnAnimate">
+                探索行程
+              </button>
+            </RouterLink>
           </div>
       </div>
       <img src="@/assets/images/personback2.png"
@@ -37,61 +40,65 @@
       好嗨遊了解你跟團不自由的感覺，也了解你自由行查資料的辛苦。
       好嗨遊讓您用最優惠的價格玩到最豐富的行程！</p>
       <RouterLink :to="`/products`" >
-        <button type="button" class="btn btn-primary text-white px-4">前往探索</button>
+        <button type="button" class="btn btn-primary text-white px-4 btnAnimate">前往探索</button>
       </RouterLink>
   </div>
-  <div class="mt-5 pt-4 justify-content-between container px-5">
+  <div class="mt-5 pt-4 justify-content-between container px-lg-5 px-2">
     <h3 class="h3 text-primary fw-bold">焦點話題</h3>
-    <div class="row pt-3 px-1 d-md-flex mx-auto justify-content-center">
-        <div class="card col-md-3 col-12 p-0 mb-md-0 mb-4 border-0 me-4 shadow"
+      <div class="row pt-3 px-1 d-md-flex mx-auto justify-content-center">
+        <div class="card col-md-3 col-12 p-0 mb-md-0 mb-4 border-0 me-md-4 me-0 shadow hover"
         v-for="product in products.slice(-3)" :key="product+123">
-        <RouterLink :to="`/product/${product.id}`" >
-          <img class="img-fluid border-0 rounded-top " :src="product.imageUrl" :alt="product">
-        </RouterLink>
-        <div class="card-body">
-          <h5 class="card-title h5 text-primary">{{product.title}}</h5>
-          <p class="card-text overflow-hidden lh-base text-start"
-          style="height: 70px;">
-            {{product.description}}
-          </p>
-          <RouterLink :to="`/product/${product.id}`">
-            <a href="#" class="btn btn-primary text-white">點我看行程</a>
+          <RouterLink :to="`/product/${product.id}`" class="text-decoration-none">
+              <img class="img-fluid border-0 rounded-top " :src="product.imageUrl" :alt="product">
+              <div class="card-body">
+                <h5 class="card-title h5 text-primary">{{product.title}}</h5>
+                <p class="card-text overflow-hidden lh-base text-start"
+                style="height: 70px;">
+                  {{product.description}}
+                </p>
+                <RouterLink :to="`/product/${product.id}`">
+                  <a href="#" class="btn btn-primary text-white">點我看行程</a>
+                </RouterLink>
+              </div>
           </RouterLink>
         </div>
-        </div>
-    </div>
+      </div>
   </div>
-
-  </div>
+</div>
 
   <div class="container mt-5 pt-4 pb-4 justify-content-between">
       <h2 class="h2 text-primary fw-bold">我猜你想找...</h2>
       <ul class="d-md-flex justify-content-around mt-3 align-middle">
-        <li class="circleBox circletxt1 my-0px mx-auto ">
-          <RouterLink :to="`/products?category=文化探索`">
-            <img src="@/assets/images/circle-walk.png" alt="" class="circleImg ">
+        <li class="circleBox my-0px mx-auto rounded-circle">
+          <RouterLink :to="`/products?category=文化探索`"
+            class="img-fluid d-block walkImg h-100 w-100 text-decoration-none">
+            <div class="coverTxtBox">
+              <span class="fs-2 fw-bold text-warp text-white">
+                文化探索
+              </span>
+            </div>
           </RouterLink>
         </li>
-        <li class="circleBox  circletxt2 my-0px mx-auto ">
+        <!-- <li class="circleBox rounded-circle my-0px mx-auto ">
           <RouterLink :to="`/products?category=美食之旅`">
-            <img src="@/assets/images/circle-food.png" alt="" class="circleImg">
+            <img src="@/assets/images/circle-food.png" alt="" class="h-100 coverTxtBox">
           </RouterLink>
         </li>
-        <li class="circleBox  circletxt3 my-0px mx-auto ">
+        <li class="circleBox rounded-circle my-0px mx-auto ">
           <RouterLink :to="`/products?category=親子出遊`">
-            <img src="@/assets/images/circle-family.png" alt="" class="circleImg">
+            <img src="@/assets/images/circle-family.png" alt="" class="h-100 coverTxtBox">
           </RouterLink>
         </li>
-        <li class="circleBox  circletxt4 my-0px mx-auto ">
+        <li class="circleBox rounded-circle my-0px mx-auto ">
           <RouterLink :to="`/products?category=休閒渡假`">
-            <img src="@/assets/images/circle-culture.png" alt="" class="circleImg">
+            <img src="@/assets/images/circle-culture.png" alt="" class="h-100 coverTxtBox">
           </RouterLink>
         </li>
-        <li class="circleBox  circletxt5 my-0px mx-auto ">
+        <li class="circleBox rounded-circle my-0px mx-auto ">
           <RouterLink :to="`/products?category=自然景色`">
-            <img src="@/assets/images/circle-nature.png" alt="" class="circleImg">
+            <img src="@/assets/images/circle-nature.png" alt="" class="h-100 coverTxtBox">
           </RouterLink>
-        </li>
+        </li> -->
       </ul>
     </div>
 
@@ -176,77 +183,58 @@ export default {
 .introBox{
   width: 40%;
 }
-.circleBox{
-    position: relative;
-    width: 160px;
-    height: 160px;
-    margin: 10px;
-    border-radius: 50%;
-    overflow: hidden;
+.btnAnimate:hover{
+  animation: zoom 1s ease;
+}
+.card:hover{
+   animation: swing 1s ease;
 }
 
-.circleImg{
-    height: 100%;
-    display: block;
-    object-fit: cover;
-  }
-  /* .circleBox::before {
-    content: attr(data-text);
-    position: absolute;
-    top: 50%;
-    left: 50%;
-    transform: translate(-50%, -50%);
-    background-color: rgba(6, 61, 34, 0.75);
-    width: 100%;
-    padding: 100px 0px;
-    color: white;
-    opacity: 0;
-    transition: opacity 0.6s ease;
-  } */
+/* 圓圈符號 */
+.circleBox{
+    width: 160px;
+    height: 160px;
+    overflow: hidden;
+}
+.coverTxtBox{
+  background-color:#273e3e;
+  opacity: 0;
+  margin:0px auto;
+  padding: 65px 0px;
+  overflow: hidden;
+  width: 100%;
+  height: 100%;
+  transition: opacity 0.3s;
+}
+.coverTxtBox:hover{
+  opacity: 0.75;
+}
 
-   /* .circleBox:hover::before {
-    content: attr(data-text);
-    position: absolute;
-    top: 50%;
-    left: 50%;
-    transform: translate(-50%, -50%);
-    background-color: rgba(6, 61, 34, 0.75);
-    width: 100%;
-    padding: 100px 0px;
-    color: white;
-    opacity: 1;
-  } */
-
-  /* .circletxt1:hover::before {
-    content: '雙腳萬能';
-    font-size: 30px;
-    font-weight: bold;
-  } */
-/*
-
-  .circletxt2:hover::before {
-    content: '美食饗宴';
-    font-size: 30px;
-    font-weight: bold;
-  }
-
-  .circletxt3:hover::before {
-    content: '大小攏來';
-    font-size: 30px;
-    font-weight: bold;
-  }
-
-  .circletxt4:hover::before {
-    content: '台灣文化';
-    font-size: 30px;
-    font-weight: bold;
-  }
-
-  .circletxt5:hover::before {
-    content: '自然美景';
-    font-size: 30px;
-    font-weight: bold;
-  } */
+.walkImg{
+  background-image: url('@/assets/images/circle-walk.png');
+  background-size: cover;
+}
+@keyframes  swing {
+  15% { transform:  translateX(5px) rotate(1deg);}
+  30% { transform:  translateX(-5px) rotate(-1deg); }
+  45% { transform:  translateX(2px) rotate(1deg); }
+  60% { transform:  translateX(-2px) rotate(-1deg);}
+  100% { transform:  translateX(0px); }
+}
+@keyframes  jump {
+  15% { transform:  translateY(2px) rotate(1deg);}
+  30% { transform:  translateY(-2px) rotate(-1deg); }
+  45% { transform:  translateY(2px) rotate(1deg); }
+  60% { transform:  translateY(-2px) rotate(-1deg);}
+  100% { transform:  translateY(0px); }
+}
+@keyframes  zoom  {
+  15% { transform:  scale(1.1) rotate(1deg);}
+  30% { transform:  scale(0.9) rotate(-1deg); }
+  45% { transform:  scale(1.1) rotate(1deg); }
+  60% { transform:  scale(0.9) rotate(-1deg);}
+  100% { transform:  scale(1); }
+}
   .activeTitle{
     font-size: 40px;
     line-height:1.6em;
@@ -268,27 +256,6 @@ export default {
   }
   @media(max-width: 767px){
   /*產品手機*/
-  /* ul{
-    padding:0px 36px;
-    li{
-      height: 160px;
-      overflow: hidden;
-    }
-  }
-  .circleBox{
-    position: relative;
-    margin-top: 28px;
-    overflow: hidden;
-    width: 100%;
-    height: auto;
-    border-radius:4%;
-  }
-  .circleImg{
-    width: 100%;
-    display: block;
-    object-fit: cover;
-    border-radius:4%;
-  } */
   .introBox{
   width: 90%;
   }
